1d97756aa9f3dd3bd7b955949267d26fea664ebb
s0521975 <s0521975@htw-berlin.de>
2017-12-09 01:38:33 -0800

    change basic auth
    for a test

--- 03-BasicAuth.js
+++ 03-BasicAuth.js
@@ -4,9 +4,8 @@ var url = require('url');
 
 var crypto = require("crypto");
 
-var auth = require('basic-auth')
-
 var port = process.env.PORT || 8081;
+
 
 
 http.createServer(function (request, response) {
@@ -28,10 +27,42 @@ http.createServer(function (request, response) {
     var lastname =  queryData.ln;
 
     var requestId = crypto.randomBytes(16).toString("hex");
-	
-	var credentials = auth(request);
 
-    
+
+
+    var headers = request.headers;
+
+    var authorization = headers['authorization'];
+
+   
+
+    var username = null;
+
+    var password = null;
+
+
+
+    if (authorization)
+
+    {
+
+        var tmp = authorization.split(' ');     // Split on a space, the original auth looks like  "Basic Y2hhcmxlczoxMjM0NQ==" and we need the 2nd part
+
+        var buf = new Buffer(tmp[1], 'base64'); // create a buffer and tell it the data coming in is base64
+
+        var plain_auth = buf.toString();        // read it back out as a string At this point plain_auth = "username:password"
+
+
+
+        var creds = plain_auth.split(':');      // split on a ':'
+
+        username = creds[0];
+
+        password = creds[1];
+
+    }
+
+
 
    if(request.method=='POST') 
 
@@ -49,7 +80,31 @@ http.createServer(function (request, response) {
 
                 // at this point, `body` has the entire request body stored in it as a string
 
-                response.end('Hi '+firstname+' '+lastname+'. This was an awesome request with query arguments! This is your provided e-mail address: '+body+'\n\nRequest Id: '+requestId+"\n Dein Nutzername war:"+credentials.name+"\nDein Passwort war: "+credentials.pass);
+                var responseMessage = 'Your provided e-mail address is: '+body;
+
+                
+
+                if (authorization) 
+
+                    responseMessage += '\nYour provided Authorization header is: '+authorization;
+
+                
+
+                if (username)
+
+                    responseMessage += '\nYour provided username is: '+username;
+
+
+
+                if (password)
+
+                    responseMessage += '\nYour provided password is: '+password;
+
+                
+
+                responseMessage += '\n\nRequest Id: '+requestId;
+
+                response.end(responseMessage);
 
        
 
@@ -61,7 +116,7 @@ http.createServer(function (request, response) {
 
    {       
 
-        response.end('Wow, this was an awesome '+request.method+' request. But you were asked to make a POST request!\n\nRequest Id: '+requestId); 
+        response.end('Wow, this was an awesome '+request.method+' request. But you were requestesd to make a POST request.!\n\nRequest Id: '+requestId); 
 
    }
 
